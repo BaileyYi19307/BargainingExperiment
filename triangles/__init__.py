60a59ad95173870637f66e912cfa6c23fbd2acd5
Jeong Kyu Lee <jkl499@nyu.edu>
2024-05-29 23:36:19 +0900

    round number recording correctly

--- triangles/__init__.py
+++ triangles/__init__.py
@@ -4,8 +4,9 @@ doc = """
 Your app description
 """
 
+# custom variables
 totalRounds = 5         # number of rounds being played
-random_grouping = models.BooleanField(initial = True)
+random_grouping = models.BooleanField(initial = True)          # true is random
 
 
 class C(BaseConstants):
@@ -29,22 +30,24 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    button_clicked=models.BooleanField(initial=False)
-    previousClicker=models.IntegerField(initial=0)
-    howManyInAgreement=models.IntegerField(initial=0)
-    agreementTimerStarted=models.BooleanField(initial=False)
-
+    button_clicked = models.BooleanField(initial=False)
+    previousClicker = models.IntegerField(initial=0)
+    howManyInAgreement = models.IntegerField(initial=0)
+    agreementTimerStarted = models.BooleanField(initial=False)
 
 
 class Player(BasePlayer):
     ogCurrency = models.FloatField(initial=0)
     currency = models.FloatField(initial=0)
+    custom_round_num = models.IntegerField(initial=1)       # manual round number
+
 
 # PAGES
 class WaitingRoom(WaitPage):
     group_by_arrival_time = False
     title_text = "Waiting Room"
     body_text = "Wait for everyone to join before proceeding"
+
 
 class Report(ExtraModel):
     subjectId = models.StringField(initial="")
@@ -58,13 +61,11 @@ class Report(ExtraModel):
     timestamp = models.FloatField(initial=0)
 
 
-
 class MyPage(Page):
     @staticmethod
     def live_method(player, data):
         g = player.group
-        print(f"Initial data received: {data}") 
-
+        print(f"Initial data received: {data}")
         print(f"Initial state: TimerStarted={g.agreementTimerStarted}, HowManyInAgreement={g.howManyInAgreement}")
 
         if 'participants' in data:
@@ -95,18 +96,27 @@ class MyPage(Page):
         if 'payoffs' in data:
             if data['payoffs']['5'] is g.previousClicker:
                 print(f"The previous clicker was participant{g.previousClicker}")
-                g.previousClicker=data['payoffs']['5']
-                round_num=player.round_number
-                p1_score=data['payoffs']['1']
-                p2_score=data['payoffs']['2']
-                p3_score=data['payoffs']['3']
+                g.previousClicker = data['payoffs']['5']
+                round_num = player.custom_round_num
+                p1_score = data['payoffs']['1']
+                p2_score = data['payoffs']['2']
+                p3_score = data['payoffs']['3']
                 #change 150 to the maximum amount of time the session has
                 currency_decay=100*((3-data['payoffs']['4'])/2) #decreases at a rate of 0.02 per second
                 time_stamp=150-currency_decay
                 print(f"The round is {round_num}")
                 print(f"The data is as follows:{data}")
                 print(f"There are {g.howManyInAgreement} players in agreement")
-                Report.create(subjectId=player.participant.id_in_session,round=g.round_number,subidgroup=data['payoffs']['5'], s1=p1_score, s2=p2_score, s3=p3_score, numAgreement=g.howManyInAgreement, timestamp=time_stamp)
+                Report.create(
+                    subjectId = player.participant.id_in_session,
+                    round = round_num,
+                    subidgroup = data['payoffs']['5'],
+                    s1 = p1_score,
+                    s2 = p2_score,
+                    s3 = p3_score,
+                    numAgreement = g.howManyInAgreement,
+                    timestamp = time_stamp
+                )
             currencyDecay = data['payoffs']['4']
 
             # Ensure default values if None is found
@@ -116,7 +126,7 @@ class MyPage(Page):
             p1_payoff = data['payoffs']['1']
             if p1_payoff is None:
                 p1.payoff = default_payoff
-            p1.payoff=p1_payoff
+            p1.payoff = p1_payoff
             p1.currency = round(float(p1.payoff) * currencyDecay,2)
             p1.ogCurrency = float(p1.payoff) * 3.00 
 
@@ -126,8 +136,7 @@ class MyPage(Page):
                 p2.payoff = default_payoff
             p2.payoff = p2_payoff
             p2.currency = round(float(p2.payoff) * currencyDecay,2)
-            p2.ogCurrency = float(p2.payoff) * 3.00 
-
+            p2.ogCurrency = float(p2.payoff) * 3.00
 
             p3 = g.get_player_by_id(3)
             p3_payoff = data['payoffs'].get('3')
@@ -137,9 +146,13 @@ class MyPage(Page):
             p3.currency = round(float(p3.payoff) * currencyDecay,2)
             p3.ogCurrency = float(p3.payoff) * 3.00
 
-
         if 'button_clicked' in data:
             return {0: {'button_id': data["button_id"], 'player_id': player.id_in_group}}
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        player.custom_round_num += 1
+
 
 class ResultsPage(Page):
     @staticmethod
@@ -148,12 +161,14 @@ class ResultsPage(Page):
             return {player.id_in_group: 'next'}
 
 
-
 class FinalPage(Page):
-    pass
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        player.custom_round_num = 1     # reset at the final page
 
 
 page_sequence = [WaitingRoom, MyPage, ResultsPage] * totalRounds + [FinalPage]
+
 
 print(f"Total pages: {len(page_sequence)}")
 for i, page in enumerate(page_sequence):
