07874fde733f49ea0a03eecce3a8864a32c40f6d
Bailey Yi <bzy203@nyu.edu>
2024-10-18 18:47:27 -0400

    Refactored tutorial files and made interface changes

--- settings.py
+++ settings.py
@@ -33,11 +33,6 @@ USE_POINTS = True
 
 
 ROOMS = [
-    dict(
-        name='econ101',
-        display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
-    ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
 
